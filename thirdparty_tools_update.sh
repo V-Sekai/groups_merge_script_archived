ee52b0333081b3282647d56de3890184e8623ea7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2020-10-16 14:32:51 -0700

    Rename script for uniqueness.

---
+++ thirdparty_tools_update.sh
@@ -0,0 +1,4 @@
+mkdir -p thirdparty
+curl -L https://gitlab.com/wavexx/git-assembler/-/raw/master/git-assembler -o ./thirdparty/git-assembler
+chmod +x ./thirdparty/git-assembler
+git config rerere.enabled true
