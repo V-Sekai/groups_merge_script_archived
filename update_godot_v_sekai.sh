5f04f305a03384c987fc70eddfd39d846b7a0200
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-11-27 16:33:38 -0800

    Test prs.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -81,6 +81,16 @@ git remote rm clayjohn || true
 git remote add clayjohn https://github.com/clayjohn/godot.git
 git fetch clayjohn
 # 
+git remote rm Razoric480 || true
+git remote add Razoric480 https://github.com/Razoric480/godot.git
+git fetch Razoric480
+#
+git remote rm jmb462 || true
+git remote add jmb462 https://github.com/jmb462/godot.git
+git fetch jmb462
+#
+
+
 
 
 
