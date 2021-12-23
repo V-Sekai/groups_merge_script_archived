4e1cc7d42ee1d90a4fb7e264eae53d9815c8b8b1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-23 07:24:58 -0800

    Fix save as binary.

--- update_godot_v_sekai.sh
+++ update_godot_v_sekai.sh
@@ -88,7 +88,10 @@ git remote rm Faless || true
 git remote add Faless https://github.com/Faless/godot.git
 git fetch Faless
 #
-
+git remote rm nikitalita || true
+git remote add nikitalita https://github.com/nikitalita/godot.git
+git fetch nikitalita
+#
 
 
 
